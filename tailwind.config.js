ef52ddd2bf58ccaeafa9c55182adc9f08617bdc6
Amin <119117189+am1nNP@users.noreply.github.com>
2024-03-07 15:35:23 +0300

    20%

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      padding:{
+        '2size': '15px 10px'
+      }
+    },
+    fontFamily:{
+      'iransans': 'Iranian Sans'                        
+    },
   },
   plugins: [],
 }
